dc8d50d5505eb074c2b9673c66ed6aee1f29b843
Byron Jones <byron@glob.com.au>
2023-12-19 00:15:59 +0800

    add recently created enhancements and tasks buglists (fixes #26)

---
+++ app/buglists/recent-enhancements.mjs
@@ -0,0 +1,43 @@
+import * as BugList from "buglist";
+import { _ } from "util";
+
+/* eslint-disable camelcase */
+
+export function init($container) {
+    const days = 14;
+    BugList.append({
+        id: "recent-enhancements",
+        $container: $container,
+        title: `Recent Enhancements < ${days} Days`,
+        description:
+            `Enhancements filed within the last ${days} days.\n` +
+            "Bugs with the meta keyword are ignored.\n" +
+            "Bugs are order by creation date, oldest first.\n" +
+            "Timestamp shows bug creation.",
+        query: {
+            email1: "wptsync@mozilla.bugs",
+            emailreporter1: "1",
+            emailtype1: "notequals",
+            resolution: "---",
+            keywords_type: "nowords",
+            keywords: "intermittent_failure",
+            chfield: "[Bug creation]",
+            chfieldfrom: `-${days}d`,
+            f1: "bug_type",
+            o1: "equals",
+            v1: "enhancement",
+            f2: "keywords",
+            o2: "notsubstring",
+            v2: "meta",
+        },
+        usesComponents: true,
+        augmentRow: ($row) => {
+            const $keywords = _($row, ".keywords");
+            if ($keywords.textContent === "") return;
+            $keywords.innerHTML = $keywords.textContent
+                .split(" ")
+                .map((kw) => `<span class="keyword-${kw}">${kw}</span>`)
+                .join(" ");
+        },
+    });
+}
--- app/tabs/triage.mjs
+++ app/tabs/triage.mjs
@@ -2,7 +2,9 @@ import * as BugList from "buglist";
 import * as TriageNeeded from "buglists/triage-needed";
 import * as NeedinfoEscalated from "buglists/needinfo-escalated";
 import * as NeedinfoStale from "buglists/needinfo-stale";
+import * as RecentEnhancements from "buglists/recent-enhancements";
 import * as RecentRegressions from "buglists/recent-regressions";
+import * as RecentTasks from "buglists/recent-tasks";
 import * as Blockers from "buglists/blockers-unassigned";
 import * as Criticals from "buglists/criticals-unassigned";
 import { _ } from "util";
@@ -13,6 +15,10 @@ export function initUI() {
     let $group = BugList.newGroup($content);
     TriageNeeded.init($group);
     RecentRegressions.init($group);
+
+    $group = BugList.newGroup($content);
+    RecentEnhancements.init($group);
+    RecentTasks.init($group);
 
     $group = BugList.newGroup($content);
     NeedinfoEscalated.init($group);
